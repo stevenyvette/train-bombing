9a5b380113b63751c6fe96207622ae05da693a9b
stevenyvette <gaoyile@126.com>
2016-11-29 16:01:44 +0800

    update

--- py/zhengli.py
+++ py/zhengli.py
@@ -1,9 +1,9 @@
 f=open("../pro.txt",'r')
-out=open("../zhengli.txt",'w')
+out=open("../les-zhengli.txt",'w')
 out.write('[')
 for line in f.readlines():
     tmp=line.split(',')
-    out.write('['+tmp[2]+','+tmp[6]+','+tmp[5]+'],')
+    out.write('['+tmp[3]+','+tmp[6][:-1]+','+tmp[5]+'],')
 
 out.write(']')
 
