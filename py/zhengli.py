4cddc3da4df6b9243b819143d836c62d2d1beea6
stevenyvette <gaoyile@126.com>
2016-11-30 14:49:01 +0800

    update

--- py/zhengli.py
+++ py/zhengli.py
@@ -1,9 +1,9 @@
-f=open("../pro.txt",'r')
-out=open("../les-zhengli.txt",'w')
+f=open("../train [Nodes].csv",'r')
+out=open("../train-zhengli.txt",'w')
 out.write('[')
 for line in f.readlines():
     tmp=line.split(',')
-    out.write('['+tmp[3]+','+tmp[6][:-1]+','+tmp[5]+'],')
+    out.write('['+tmp[2]+','+tmp[4][:-1]+','+tmp[3]+'],')
 
 out.write(']')
 
